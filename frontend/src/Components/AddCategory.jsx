c058b54489497a9bed4e3930a4cb0f1dfc0af31f
Muskan Kumari <muskansrivastava0305@gmail.com>
2025-05-19 10:27:35 +0530

    Updated Code

--- frontend/src/Components/AddCategory.jsx
+++ frontend/src/Components/AddCategory.jsx
@@ -51,8 +51,8 @@ const handleSubmit = async (e) => {
 }
 
   return (
-    <div className="fixed inset-0 bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-gray-100 rounded-lg p-6 w-full max-w-md">
+    <div className="fixed inset-0 bg-transparent backdrop-blur-xs bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white rounded-lg p-6 w-full max-w-md">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-xl font-bold">Add New Category</h2>
           <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
